469cd766bbc987e978228eec35899a7bc5347f84
yonid <yonid@sears.co.il>
2016-04-26 17:06:41 +0300

    change extension description

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -3,10 +3,10 @@ namespace MadsKristensen.AddAnyFile
 	static class Vsix
 	{
 		public const string Id = "2E78AA18-E864-4FBB-B8C8-6186FC865DB3";
-		public const string Name = "Add New File";
-		public const string Description = "The fastest and easiest way to add new files to any project - including files that start with a dot";
-		public const string Version = "3.1";
-		public const string Author = "Mads Kristensen";
-		public const string Tags = "file, add, template";
+		public const string Name = "VS Tests Generator";
+		public const string Description = "The fastest and easiest way to add tests for your project";
+		public const string Version = "0.1";
+		public const string Author = "Dennis Nerush & Yoni Dor";
+		public const string Tags = "tests, file , add";
 	}
 }
